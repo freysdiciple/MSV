b77a3ceeafc49c642b86f3f783479b24c17b4fb3
Massimo Angelo Hansen <massimo.hansen@gmail.com>
2021-01-07 09:17:46 +0100

    Initial

---
+++ src/sample/Main.java
@@ -0,0 +1,63 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import sample.GameScreen.BoardGenerator;
+import sample.GameScreen.TopMenu;
+import sample.MenuScreen.PregameMenu;
+
+public class Main extends Application {
+
+    private static Stage window;
+    private static Scene pregame, game;
+
+    static final int stageWidth = 1000;
+    static final int stageHeight = 600;
+
+    static final int inset = 20;
+    static final int gap = 2;
+
+    static int sizeX;
+    static int sizeY;
+
+    static int difficulty;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        window = primaryStage;
+        window.setTitle("Hello World");
+
+        pregame = new PregameMenu(new int[]{stageWidth, stageHeight}).getScene();
+
+        window.setScene(pregame);
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static void collectGameScene(){
+        BorderPane gameView = new BorderPane();
+        gameView.setTop(new TopMenu(stageWidth, window, pregame).visual());
+        gameView.setCenter(new BoardGenerator(new int[]{sizeX, sizeY, stageWidth}, new int[]{gap,inset}).visual());
+        game = new Scene(gameView);
+    }
+
+    public static void goToScene(String where){
+        window.setScene(where == "pre" ? pregame : game);
+    }
+
+    public static void setGamesize(int _sizeX){
+        sizeX = _sizeX;
+        sizeY = (stageHeight-100)*sizeX/stageWidth;
+    }
+
+    public static void setDifficulty(int _difficulty){
+        difficulty = _difficulty;
+    }
+}
---
+++ src/sample/Tile.java
@@ -0,0 +1,53 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Tile {
+    private double width;
+    private double height;
+
+    private int xpos;
+    private int ypos;
+
+    private Rectangle tile;
+
+    private final String tileBasicURL = "images/tile-reg.png";
+    private final String tileHoverURL = "images/tile-hov.png";
+
+    private String value;
+
+    public Tile(double[] dims, int[] pos, String value){
+        this.width = dims[0];
+        this.height = dims[1];
+
+        this.xpos = pos[0];
+        this.ypos = pos[1];
+
+        this.value = value;
+
+        tile = new Rectangle(width, height);
+        GridPane.setConstraints(tile, pos[0], pos[1]);
+
+        initialConfig();
+    }
+
+    public void initialConfig(){
+        Image tileBasic = new Image(tileBasicURL);
+        Image tileHover = new Image(tileHoverURL);
+        tile.setFill(new ImagePattern(tileBasic));
+        tile.setOnMouseEntered(e -> tile.setFill(new ImagePattern(tileHover)));
+        tile.setOnMouseExited(e -> tile.setFill(new ImagePattern(tileBasic)));
+    }
+
+    public Rectangle visual(){
+        return tile;
+    }
+}
